e42ce7395a0ffef640bcb875ad17424c3a415069
Rakesh Katti <rakeshkatti@Rakeshs-Air.Dlink>
2020-09-11 23:48:43 +0530

    Updating packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Work in progress.. Come back in few days. :) 
-        </p>
+        <p>Work in progress.. Come back in few days. :)</p>
       </header>
     </div>
   );
